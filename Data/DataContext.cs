c56710022a3e83346d5d7977ecf4062634dd19ba
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-19 14:48:42 -0400

    Added the ctor and dbset.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,7 +1,12 @@
-﻿namespace DatingApp2.API.Data
+﻿using DatingApp2.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp2.API.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
     }
 }
