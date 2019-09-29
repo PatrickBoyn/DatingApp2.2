388efd3c35eeb8c01c13eb0a12f3b625a351803f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-29 01:48:47 -0400

    Added the properties for the user mode.

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,9 @@
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
