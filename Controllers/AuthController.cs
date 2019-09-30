a437fe67a3bc74aba37d17474c185215f826fa18
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-30 14:27:11 -0400

    Added the register controller.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,17 +1,37 @@
-﻿using DatingApp2.API.Data;
+﻿using System.Threading.Tasks;
+using DatingApp2.API.Data;
+using DatingApp2.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp2.API.Controllers
 {
     [Route("ap/[controller]")]
     [ApiController]
-    public class AuthController 
+    public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
 
         public AuthController(IAuthRepository repo)
         {
             _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            username = username.ToLower();
+
+            if (await _repo.UserExists(username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                UserName = username
+            };
+
+            User createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
         }
     }
 }
