4663036db754fe70835fdbc1f7ed918fb1bb512c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-30 17:53:11 -0400

    Added validation with annotations.

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -1,8 +1,16 @@
-﻿namespace DatingApp2.API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp2.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
+        [MinLength(2, 
+            ErrorMessage = "Your Username must contain at least two characters.")]
         public string UserName { get; set; }
+        [Required]
+        [MinLength(6, 
+            ErrorMessage = "Your password must be at least 6 characters long.")]
         public string Password { get; set; }
     }
 }
