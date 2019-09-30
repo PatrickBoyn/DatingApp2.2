385aa8332d1b8467212b05908ac089caec81b722
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-30 01:27:45 -0400

    Added the ctor, and implemented the members of the interface.

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,7 +1,30 @@
-﻿namespace DatingApp2.API.Data
+﻿using System.Threading.Tasks;
+using DatingApp2.API.Models;
+
+namespace DatingApp2.API.Data
 {
-    public class AuthRepository
+    public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
         
+        public async Task<User> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
