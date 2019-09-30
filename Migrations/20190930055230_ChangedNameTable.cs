48ff7e78f9bce1664405dedc409316dd98625ec5
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-30 02:07:07 -0400

    Updated the table name.

---
+++ Migrations/20190930055230_ChangedNameTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp2.API.Migrations
+{
+    public partial class ChangedNameTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "UserName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
